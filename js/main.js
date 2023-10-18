9cee5bda249b033b6114676f557db3bd223a7823
ChamithPrasanna <chamith@minthrm.net>
2023-10-18 20:18:16 +0530

    new developmnt

--- js/main.js
+++ js/main.js
@@ -10,8 +10,29 @@ function print(){
 }
 
 function getdata(){
-    var email = document.getElementById("email").value;
+    var email = document.getElementById("emails").value;
     //console.log(email);
     document.getElementById("result").innerHTML=email;
+
+}
+
+function getsubjectData(){
+    var sub1 = document.getElementById("sub1").value;
+    var sub2 = document.getElementById("sub2").value;
+    var sub3 = document.getElementById("sub3").value;
+    var sub4 = document.getElementById("sub4").value;
+    var sub5 = document.getElementById("sub5").value;
+
+    var subTotal = Number(sub1) + Number(sub2) + Number(sub3) + Number(sub4) + Number(sub5) ;
+    var avgMarks = subTotal/5;
+    if(subTotal > 50){
+        document.getElementById("passFail").innerHTML= "Pass";
+    }else{
+        document.getElementById("passFail").innerHTML= "Fail";
+    }
+
+
+    document.getElementById("subTot").innerHTML= subTotal;
+    document.getElementById("avgMarks").innerHTML= avgMarks;
 
 }
